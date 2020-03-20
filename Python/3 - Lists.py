0eb1ded5195d7da41794666e73e4c30a84dd8212
rcsraymer <rcs.raymer@gmail.com>
2020-03-20 10:34:12 -0400

    Create 3 - Lists.py

---
+++ Python/3 - Lists.py
@@ -0,0 +1,50 @@
+ListA = [3,5,9]
+
+
+
+
+# pull values by index
+value = ListA[2]
+
+
+
+
+# slice
+ListB = ListA[0:2]
+
+
+
+
+# append
+ListA.append(11)
+
+
+
+
+# Embedding Lists
+ListC = ['Charlotte',ListA]
+
+
+
+
+# pull values by index - embedded
+TheValueThree = ListC[1][0]
+
+
+
+
+"""
+append()	Adds an element at the end of the list
+clear()	Removes all the elements from the list
+copy()	   Returns a copy of the list
+count()	Returns the number of elements with the specified value
+extend()	Add the elements of a list (or any iterable), to the end of the current list
+index()	Returns the index of the first element with the specified value
+insert()	Adds an element at the specified position
+pop()	   Removes the element at the specified position
+remove()	Removes the item with the specified value
+reverse()	Reverses the order of the list
+sort()	    Sorts the list
+
+"""
+
